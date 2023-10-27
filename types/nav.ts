27053c75f56db316c2ec2ee44b58ebd54e51ee8f
Huse Kivrak <huse7@pm.me>
2023-10-27 17:26:46 -0400

    types for nav, quiz - the latter will change with serializers in backend

---
+++ types/nav.ts
@@ -0,0 +1,4 @@
+export interface NavLink {
+  name: string;
+  href: string;
+}
---
+++ types/quiz.ts
@@ -0,0 +1,28 @@
+export interface Quiz {
+  author: number;
+  description: string;
+  is_published: boolean;
+  questions: Question[];
+  slug: string;
+  title: string;
+  uuid: string;
+}
+
+export interface Question {
+  answers: Answer[];
+  author: number;
+  order: number;
+  question_type: string;
+  quiz: number;
+  slug: string;
+  text: string;
+  uuid: string;
+}
+
+export interface Answer{
+  is_correct: boolean
+  order: number
+  question: number
+  text: string
+  uuid: string
+}
