ca3b8c7029514fd0816a6309425214eeaead5585
Huse Kivrak <huse7@pm.me>
2023-10-27 17:29:05 -0400

    cleaned up a bit, added nav

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,19 @@
 import { useAuth } from '@/context/AuthContext';
 import LoginCard from '@/components/LoginCard';
 import Dashboard from '@/components/Dashboard';
-
+import NavBar from '@/components/UI/NavBar';
+import { NAV_LINKS } from '@/lib/constants';
 export default function LandingPage() {
-  const {user, isLoading} = useAuth();
+  const { user, isLoading } = useAuth();
 
-  if (isLoading){
-    return <div>Loading..</div>
+  if (isLoading) {
+    return <div>Loading..</div>;
   }
 
-  return <div>{user ? <Dashboard /> : <LoginCard />}</div>;
+  return (
+    <div>
+      <NavBar navLinks={NAV_LINKS} />
+      {user ? <Dashboard /> : <LoginCard />}
+    </div>
+  );
 }
