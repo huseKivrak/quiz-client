1ae9d0f3999c8cb64feec5d5c7b4c9e56d5d7f47
Huse Kivrak <huse7@pm.me>
2023-10-27 16:31:47 -0400

    spa refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,15 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { getAndSetUser } from '@/utils/authUtils';
+
 import { useAuth } from '@/context/AuthContext';
+import LoginCard from '@/components/LoginCard';
+import Dashboard from '@/components/Dashboard';
 
 export default function LandingPage() {
-  const router = useRouter();
-  const authContext = useAuth();
+  const {user, isLoading} = useAuth();
 
-  useEffect(() => {
-    getAndSetUser(authContext).then((user) => {
-      if (user) {
-        router.push('/dashboard');
-      }
-    });
-  }, []);
+  if (isLoading){
+    return <div>Loading..</div>
+  }
 
-  return (
-    <div>
-      <h1>Welcome to My App</h1>
-      {!authContext.user && (
-        <>
-          <h2>Hello, Guest!</h2>
-          <p>You are not logged in.</p>
-        </>
-      )}
-    </div>
-  );
+  return <div>{user ? <Dashboard /> : <LoginCard />}</div>;
 }
