4c6aabf6e7b05634c9914fa4e1c45dd166aa79b6
Huse Kivrak <huse7@pm.me>
2023-10-27 17:28:36 -0400

    boilerplate nav, footer

---
+++ components/UI/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <footer className='footer footer-center p-4 bg-base-100 text-base-content'>
+        <aside>
+          <p>Copyright © 2023 - All right reserved.</p>
+        </aside>
+      </footer>
+    );
+  }
---
+++ components/UI/NavBar.tsx
@@ -0,0 +1,64 @@
+import { NavLink } from '@/types/nav';
+import Link from 'next/link';
+
+interface NavBarProps {
+  navLinks: NavLink[];
+}
+
+function NavBar({ navLinks }: NavBarProps) {
+  return (
+    <div className='navbar bg-secondary'>
+      <div className='navbar-start flex items-center'>
+        <Link href='/' className='btn btn-ghost rounded-none'>
+          quiz
+        </Link>
+        <div className='divider divider-horizontal mx-2' />
+      </div>
+      <div className='navbar-center hidden lg:flex'>
+        <ul className='menu menu-horizontal'>
+          {navLinks.map((link) => (
+            <li key={link.href} className='px-10'>
+              <Link href={link.href}>
+                <span>{link.name}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='navbar-end'>
+        <div className='dropdown'>
+          <label tabIndex={0} className='btn btn-ghost lg:hidden'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h8m-8 6h16'
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className='menu menu-md dropdown-content p-0 mt-3 z-[1] shadow bg-base-100 w-52 right-0'
+          >
+            {navLinks.map((link) => (
+              <li className='text-center' key={link.href}>
+                <Link href={link.href}>
+                  <span>{link.name}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
