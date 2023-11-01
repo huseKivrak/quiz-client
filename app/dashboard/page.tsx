16b4f0b0a60d1e4c49c9f6ea58cf19bdccd3a802
Huse Kivrak <huse7@pm.me>
2023-10-31 21:27:26 -0400

    added redirect if !user, minor format experimenting

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from '@/contexts/AuthContext';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { authFetch } from '@/utils/authUtils';
 import { API_QUIZZES_URL } from '@/lib/apiConstants';
-import { Quiz } from '@/types/quiz';
+import { Quiz } from '@/types/api/quiz';
 import Link from 'next/link';
 
 const Dashboard = () => {
   const { user, isLoading } = useAuth();
   const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchQuizzes = async () => {
@@ -23,26 +25,34 @@ const Dashboard = () => {
     fetchQuizzes();
   }, [user, isLoading]);
 
-  if (isLoading || !user) {
+  if (isLoading) {
     return <div>Loading...</div>;
   }
+  if (!user) {
+    router.push('/');
+    return <div>Redirecting...</div>;
+  }
   return (
-    <div className='bg-primary min-h-screen p-6'>
-      <h1 className='text-4xl mb-4'>Quiz Dashboard</h1>
+    <div className='bg-primary p-8 mt-4 rounded-2xl'>
+      <h1 className='text-4xl mb-4 tracking-widest'>quiz dashboard</h1>
 
       <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-        <div className='bg-white p-6 rounded-lg shadow-md'>
+        <div className='bg-white p-6 rounded-lg shadow-md h-min tracking-widest'>
           <h2 className='text-2xl mb-4'>{`${user.firstName} ${user.lastName}`}</h2>
           <p className='italic'>({user.username})</p>
+          <p className='italic tracking-widest text-sm mt-2'>{user.email}</p>
         </div>
 
         <div className='bg-white p-6 rounded-lg shadow-md'>
-          <h2 className='text-2xl mb-4'>My Quizzes</h2>
+          <h2 className='text-2xl mb-4'>quizzes</h2>
           {quizzes ? (
             <ul>
               {quizzes.map((quiz) => (
-                <li key={quiz.slug}>
-                  <Link href={`/quizzes/${quiz.slug}`}>{quiz.title}</Link>
+                <li key={quiz.slug} className='pb-2'>
+                  <Link href={`/quizzes/${quiz.slug}`}>
+                    {quiz.title}
+                    <p className='text-xs'>{quiz.description}</p>
+                  </Link>
                 </li>
               ))}
             </ul>
