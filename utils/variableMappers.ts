63ce76ae9fd20ab209faaa455e72e7461b3967c9
Huse Kivrak <huse7@pm.me>
2023-11-02 00:52:25 -0400

    new version: two-way conversions, caches

--- utils/variableMappers.ts
+++ utils/variableMappers.ts
@@ -1,51 +1,86 @@
-export type DataType = { [key: string]: string | number | DataType[] };
+export type DataValue = string | number | DataObject | DataArray;
+export type DataObject = { [key: string]: DataValue };
+export type DataArray = DataObject[];
 
 /**
- Special conversions
- {
-  api value : 'front-end value'
- }
+ * Differently named special variables.
+ * {
+ * 'api_name': 'clientName'
+ * };
  */
-
-
-const varDict: { [key: string]: string } = {
+const snakeToCamelSpecialCases: { [key: string]: string } = {
   pk: 'id',
 };
 
-const camelCaseCache: Record<string, string> = {};
+const camelToSnakeSpecialCases = Object.fromEntries(
+  Object.entries(snakeToCamelSpecialCases).map(([key, value]) => [value, key])
+);
 
-export function convertToCamelCase(str: string): string {
-  const lowerStr = str.toLowerCase();
-  if (camelCaseCache[lowerStr]) return camelCaseCache[lowerStr];
+const camelCaseResultsCache: Record<string, string> = {};
+const snakeCaseResultsCache: Record<string, string> = {};
 
-  const converted = lowerStr.replace(/_([a-z])/g, (_, p1) => p1.toUpperCase());
-  camelCaseCache[lowerStr] = converted;
+export function toCamelCase(str: string): string {
+  if (camelCaseResultsCache[str]) return camelCaseResultsCache[str];
+
+  const converted = str.replace(/_([a-z])/g, (_, p1) => p1.toUpperCase());
+  camelCaseResultsCache[str] = converted;
+  snakeCaseResultsCache[converted] = str;
 
   return converted;
 }
 
-export function transformKeys(obj: DataType): DataType {
-  const newObj: DataType = {};
+export function toSnakeCase(str: string): string {
+  if (snakeCaseResultsCache[str]) return snakeCaseResultsCache[str];
 
-  for (const [key, val] of Object.entries(obj)) {
-    const newKey = varDict[key] || convertToCamelCase(key);
+  const converted = str.replace(
+    /[A-Z]/g,
+    (letter) => `_${letter.toLowerCase()}`
+  );
+  camelCaseResultsCache[converted] = str;
+  snakeCaseResultsCache[str] = converted;
 
-    if (Array.isArray(val)) {
-      newObj[newKey] = camelCaseMapper(val as DataType[]) as DataType[];
-    } else if (typeof val === 'string') {
-      newObj[newKey] = val;
-    }
-  }
-
-  return newObj;
+  return converted;
 }
 
-export default function camelCaseMapper(
-  data: DataType | DataType[]
-): DataType | DataType[] {
-  if (Array.isArray(data)) {
-    return data.map((singleData) => transformKeys(singleData));
-  } else {
-    return transformKeys(data);
+/**
+ *
+ * @param input : object or array of objects
+ * @param convertedCase : 'camel' or 'snake'
+ * @returns : object or array of objects with keys converted to the specified case
+ */
+function transformObjectOrArrayKeys(
+  input: DataArray | DataObject,
+  convertedCase: 'camel' | 'snake' = 'camel'
+): DataValue {
+  if (Array.isArray(input)) {
+    return input.map(
+      (item) => transformObjectOrArrayKeys(item, convertedCase) as DataObject
+    );
+  } else if (typeof input === 'object' && input !== null) {
+    const newObj: DataObject = {};
+    for (const [key, val] of Object.entries(input)) {
+      const newKey =
+        convertedCase === 'camel'
+          ? snakeToCamelSpecialCases[key] || toCamelCase(key)
+          : camelToSnakeSpecialCases[key] || toSnakeCase(key);
+
+      if (typeof val === 'object') {
+        newObj[newKey] = transformObjectOrArrayKeys(val, convertedCase);
+      } else {
+        newObj[newKey] = val;
+      }
+    }
+    return newObj;
   }
+  return input;
+}
+
+export function deepTransformKeys(
+  input: DataObject | DataArray | string | number,
+  convertedCase: 'camel' | 'snake' = 'camel'
+): DataObject | DataArray | string | number {
+  if (typeof input === 'object')
+    return transformObjectOrArrayKeys(input, convertedCase);
+
+  return input;
 }
