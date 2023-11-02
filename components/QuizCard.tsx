a8b00559eb6c18c663a0a472ea1f55f34c7b8e32
Huse Kivrak <huse7@pm.me>
2023-11-02 00:51:03 -0400

    trying out question cards

--- components/QuizCard.tsx
+++ components/QuizCard.tsx
@@ -1,16 +1,24 @@
 import { Quiz } from '@/types/api/quiz';
-import QuestionCard from './QuestionCard';
+import MultipleChoiceQuestionCard from './attempts/MultipleChoiceQuestionCard';
+import TrueFalseQuestionCard from './attempts/TrueFalseQuestionCard';
+
 export default function QuizCard({ quiz }: { quiz: Quiz }) {
   return (
     <div>
-      <h1>{quiz.title}</h1>
+      <h1 className='tracking-widest font-light text-3xl mt-12'>
+        {quiz.title}
+      </h1>
       <p>{quiz.description}</p>
-      <h2>Questions</h2>
-      <ul>
+
+      <ul className=''>
         {quiz.questions.map((question) => (
-            <ul>
-                <QuestionCard question={question} />
-            </ul>
+          <li key={question.id} className='my-2'>
+            {question.questionType === 'true_false' ? (
+              <TrueFalseQuestionCard question={question} />
+            ) : (
+              <MultipleChoiceQuestionCard question={question} />
+            )}
+          </li>
         ))}
       </ul>
     </div>
